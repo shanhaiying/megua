ae1f274a7d829f1102c7dda0942e56cfe31a8e5b
Pedro Cruz <pedrocruzaveiro@gmail.com>
2012-12-19 14:20:23 +0000

    added megout.py

---
+++ megua/megout.py
@@ -0,0 +1,30 @@
+
+
+
+class MegOutput
+
+
+    def output_init(self, bookbase, 
+    def output_save(self, bookbase, )
+        -> exercise was not saved on database
+        -> exercise changed on database
+        -> was saved
+
+    def output_pythontest	
+
+
+    def output_latexcompilation
+
+
+    def search
+
+    def print_instance
+     
+          - commandline + prettyprint
+          - notebook + mathjax
+          - notebook + text + html
+
+
+class MegNotebook(MegOutput)
+
+
