01472b76885d13c7b10c0781a89c784ab7f6517e
Pedro Cruz <pedrocruzaveiro@gmail.com>
2014-03-09 22:37:53 +0000

    Recover old xsphinx.py

--- megua/cr.py
+++ megua/cr.py
@@ -12,5 +12,32 @@ LINKS:
 
 """
 
+"""
+import rpy2
+import rpy2.robjects as robjects
+stemf = robjects.r['stem']
+runif = robjects.r['runif']
+rprint =robjects.r['print'] 
+buf = []
+def f(x):
+    # function that append its argument to the list 'buf'
+    buf.append(x)
+
+# output from the R console will now be appended to the list 'buf'
+rpy2.rinterface.setWriteConsole(f)
+
+stemf( runif(int(100)) )
+
+rprint( runif(int(100)) )
+
+#date = rpy2.rinterface.baseenv['date']
+#rprint = rpy2.rinterface.baseenv['print']
+#rprint("date()")
+
+# the output is in our list (as defined in the function f above)
+print(buf)
+
+print ''.join(buf)
 
 
+"""
